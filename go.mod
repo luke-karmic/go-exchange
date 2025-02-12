c3a9ca5f96d4332bacc6f6faf4afd6de4cc5111d
Luke Taaffe <luketaaffese@gmail.com>
2025-02-12 21:04:12 +0700

    Set up project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/luke-karmic/go-exchange
+
+go 1.23.6
---
+++ orderbook.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Orderbook struct {
+	Asks []*Limit
+	Bids []*Limit
+
+	AskLimits map(float64) *Limit
+	BidLimits map(float64) *Limit
+}
+
+type Limit struct {
+	Price       float64
+	Orders      []*Order
+	TotalVolume float64
+}
+
+type Order struct {
+	Size      float64
+	Limit     *Limit
+	Timestamp int64
+}
+
+func NewOrder(bid bool, size float64) *Order {
+	return &Order{
+		Size:      size,
+		Timestamp: time.Now().UnixNano(),
+	}
+}
+
+func NewLimitOrder(price float64) *Limit {
+	return &Limit{
+		Price:  price,
+		Orders: []*Order{},
+	}
+}
+
+func (o *Order) String() string {
+	return fmt.Sprintf("[size %.2f]", o.Size)
+}
+
+func (l *Limit) AddOrder(o *Order) {
+	o.Limit = l
+	l.Orders = append(l.Orders, o)
+	l.TotalVolume += o.Size
+}
+
+func (l *Limit) DeleteOrder(o *Order) {
+	for i := 0; i < len(l.Orders); i++ {
+		if l.Orders[i] == o {
+			l.Orders[i] = l.Orders[len(l.Orders)-1]
+			l.Orders = l.Orders[:len(l.Orders)-1]
+		}
+	}
+	o.Limit = nil
+	l.TotalVolume -= o.Size
+
+	// TODO: Re-sort all resting orders
+}
+
+func NewOrderBook() *OrderBook {
+	return &OrderBook{
+		Asks: []*Limit{},
+		Bids: []*Limit{},
+		AskLimits: make(map[float64]*Limit),
+		BidLimits: make(map[float64]*Limit),
+	}
+}
